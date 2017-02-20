b5d1d57cff147d7942a139dc71d96fa79abadacb
Arthur Caillau <arthur.caillau@airbnb.com>
2017-02-20 12:25:04 -0800

    [Data] add show instance to Data

--- src/Data.hs
+++ src/Data.hs
@@ -1,9 +1,21 @@
 module Data where
 
+import           Data.List (intercalate)
+
 data LispVal = Atom String
              | List [LispVal]
              | DottedList [LispVal] LispVal
              | Number Integer
              | String String
              | Bool Bool
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show LispVal where
+  show (Atom string) = string
+  show (List xs) = "(" ++ (intercalate " " (map show xs)) ++ ")"
+  show (DottedList hs t) = "(" ++ (intercalate " " (map show hs)) ++ " . " ++ show t ++ ")"
+  show (Number k) = show k
+  show (String s) = s
+  show (Bool b) = case b of
+                    True  -> "#t"
+                    False -> "#f"
